d0654ff6aed2e795eb1384896a5407b0bc8d4d47
Tyler Allen <tyler.allen@hashicorp.com>
2023-04-06 18:46:45 +0100

    Update Index

--- Client/index.js
+++ Client/index.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 
-axios.get('http://127.0.0.1:3000')
+axios.get('https://localhost:8443')
     .then(function (response) {
         // handle success
         console.log(response.status);
     })
     .catch(function (error) {
         // handle error
-        console.log("error");
+        console.log(error);
     })
     .finally(function () {
         // always executed
